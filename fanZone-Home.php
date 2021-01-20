421cda34d5851645a6bdf4fa397083cf1f58291d
Gurveer-C1331 <45864502+Gurveer-C1331@users.noreply.github.com>
2021-01-20 17:55:03 -0500

    Update fanZone-Home.php

--- fanZone-Home.php
+++ fanZone-Home.php
@@ -87,7 +87,7 @@
     </div>
     
     <?php
-      $mysqli = new mysqli("localhost", "g3chahal", "Cytyoin/", "g3chahal");
+      $mysqli = new mysqli("localhost", "USERNAME", "PASSWORD", "USERNAME");
 
       if ($mysqli -> connect_errno) {
         echo "Failed connection to: ".$mysqli -> connect_errno;
